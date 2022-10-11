88c67dee959532842b87339a93d3ee6a408218d7
Zak Freeman <zakfreeman12@gmail.com>
2022-10-11 11:13:13 -0500

    Moved stat generation to separate file.

---
+++ stats.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from tkinter import ttk
+
+padding = {'padx': 5, 'pady': 5}
+
+def statEntry(stat, col, row, window, frame):
+    """Creates the frames and Entry objects for the various stats of a character.
+    Returns the Entry Field object or Checkbutton object"""
+    frm_stat = ttk.Frame(frame, borderwidth=5, relief=GROOVE)
+    frm_stat.grid(column=col, row=row, **padding)
+    lbl_stat = Label(frm_stat, text=stat)
+    lbl_stat.grid(column=col, row=row, **padding)
+    if stat == 'INSPIRATION':
+        window.chkVar = IntVar()
+        window.chkVar.set(0)
+        chk_stat = ttk.Checkbutton(frm_stat, variable=window.chkVar, text='Advantage')
+        chk_stat.grid(column=col, row=row+1)
+        return chk_stat
+    vcmd = (window.register(statValidate), '%P')
+    ent_stat = Entry(
+        frm_stat,
+        width=4,
+        validate='key',
+        validatecommand=vcmd
+    )
+    ent_stat.grid(column=col, row=row+1, **padding, sticky='N')
+    return ent_stat
+
+def statValidate(P):
+    """Validate inputs for the stat objects. Takes Digits only."""
+    if len(P) == 0 or (len(P) > 0 and P.isdigit()):
+        return True
+    else:
+        return False
