d028d02c6ddf1eca8c389d9a1e17b2fc25e9313d
Zak Freeman <zakfreeman12@gmail.com>
2022-10-11 13:19:16 -0500

    Formatting

--- char_p_l.py
+++ char_p_l.py
@@ -1,13 +1,16 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter.scrolledtext import ScrolledText
 
 def otherProfs(frame, col, row, name):
     lbl_name = Label(frame, text=name)
-    lbl_name.grid(column=col, row=row)
-    txt_prof = Text(
+    lbl_name.grid(column=col, row=row+1)
+    txt_prof = ScrolledText(
         frame,
         height=3,
-        width=20
+        width=15,
+        wrap=WORD,
         )
-    txt_prof.grid(column=col, row=row+1, padx=5, pady=5)
+    txt_prof.grid(column=col, row=row+2, padx=5, pady=5)
+
     return txt_prof
