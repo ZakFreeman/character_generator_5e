79ad2175c872641366efe090309290f7cf48ff84
Zak Freeman <zakfreeman12@gmail.com>
2022-10-13 15:29:37 -0500

    Implement data saving to json files.

--- buttons.py
+++ buttons.py
@@ -1,22 +1,56 @@
 import json
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 
 def saveButton(savedDicts):
-    basicDict = savedDicts['basicinfo']
-    with open('saveData/basicinfo.txt', 'w') as f:
-        basic = {}
-        for label in basicDict:
-            basic[label] = basicDict[label][1].get()
-        jbasic = json.dumps(basic)
-        f.write(jbasic)
-    
-    dsaveDict = savedDicts['deathsaves']  # TODO
-    with open('saveData/deathsaves.txt', 'w') as f:
-        dsave = {}
-        for save in dsaveDict:  # 'Successes', 'Failures'.
-            for chk in dsaveDict[save]:  # 3 Checkboxes each.
-                dsave[save] = chk.get()
-        f.write(json.dumps(dsave))
-
-    abilDict = savedDicts['abilities']
-    
+    saveNamesList = []  # Used to pass names for file generation.
+    for name in savedDicts:
+        saveNamesList.append(name)
+    labelEntrySave(savedDicts[saveNamesList[0]], saveNamesList[0])
+    labelMulEntrySave(savedDicts[saveNamesList[1]], saveNamesList[1])
+    labelEntrySave(savedDicts[saveNamesList[2]], saveNamesList[2])
+    statSave(savedDicts[saveNamesList[3]], saveNamesList[3])
+    statSave(savedDicts[saveNamesList[4]], saveNamesList[4])
+    labelMulEntrySave(savedDicts[saveNamesList[5]], saveNamesList[5])
+    labelEntrySave(savedDicts[saveNamesList[6]], saveNamesList[6])
     return
+
+
+def labelEntrySave(dict, name):
+    """Handles save data generation for dictionaries that contain a label, 
+    then Entry."""
+    with open(f'saveData/{name}.txt', 'w') as f:
+        basicInfo = {}  # Remake dict as {Label : Entry.get()}
+        for label in dict:
+            basicInfo[label] = dict[label][1].get()
+        jbasic = json.dumps(basicInfo)
+        f.write(jbasic)
+    return jbasic
+
+
+def labelMulEntrySave(dict, name):
+    """Handles save data generation for the death save dictionary."""
+    with open(f'saveData/{name}.txt', 'w') as f:
+        mulEntry = {}  # Remake dict as {Label : [List of values]}
+        for label in dict:  # 'Successes', 'Failures'.
+            vList = []
+            for chk in dict[label]:  # Multiple values
+                vList.append(chk.get())
+            mulEntry[label] = vList
+        jMul = json.dumps(mulEntry)
+        f.write(jMul)
+    return jMul
+
+
+def statSave(dict, name):
+    """Handles save data for stats, and for the proficiencies text boxes."""
+    with open(f'saveData/{name}.txt', 'w') as f:
+        statInfo = {}  # Remake dict as {Label : Entry.get() or Checkbutton.get()}
+        for stat in dict:
+            if isinstance(dict[stat], ScrolledText):  # Handles the proficiencies.
+                statInfo[stat] = dict[stat].get('1.0', END)
+            else:  # Handles Stats.
+                statInfo[stat] = dict[stat].get()
+        jstats = json.dumps(statInfo)
+        f.write(jstats)
+    return jstats
