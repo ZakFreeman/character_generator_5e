973c7dbadaded3393e02f60231122a4c22eda77e
Zak Freeman <zakfreeman12@gmail.com>
2022-10-13 10:07:37 -0500

    WIP

--- buttons.py
+++ buttons.py
@@ -1,15 +1,22 @@
+import json
+
 def saveButton(savedDicts):
     basicDict = savedDicts['basicinfo']
     with open('saveData/basicinfo.txt', 'w') as f:
+        basic = {}
         for label in basicDict:
-            f.write(basicDict[label][1].get() + '\n')
+            basic[label] = basicDict[label][1].get()
+        jbasic = json.dumps(basic)
+        f.write(jbasic)
     
-    dsaveDict = savedDicts['deathsaves']
+    dsaveDict = savedDicts['deathsaves']  # TODO
     with open('saveData/deathsaves.txt', 'w') as f:
-        for save in dsaveDict:
-            for chk in dsaveDict[save]:
-                if chk.get():
-                    f.write('1\n')
-                else:
-                    f.write('0\n')
+        dsave = {}
+        for save in dsaveDict:  # 'Successes', 'Failures'.
+            for chk in dsaveDict[save]:  # 3 Checkboxes each.
+                dsave[save] = chk.get()
+        f.write(json.dumps(dsave))
+
+    abilDict = savedDicts['abilities']
+    
     return
