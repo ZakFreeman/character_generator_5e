95c8f2f505a589ba5def4ee0e06a31c7468c7f98
Zak Freeman <zakfreeman12@gmail.com>
2022-10-14 14:32:51 -0500

    Added abilitySave() to handle checkbox.

--- buttons.py
+++ buttons.py
@@ -3,18 +3,34 @@ from tkinter import *
 from tkinter.scrolledtext import ScrolledText
 
 def saveButton(savedDicts):
+    """Calls functions to handle the dictionaries passed from the save button call.
+    The dictionaries are all different, and need to be handled a few different
+    ways."""
     saveNamesList = []  # Used to pass names for file generation.
     for name in savedDicts:
         saveNamesList.append(name)
     labelEntrySave(savedDicts[saveNamesList[0]], saveNamesList[0])  # Basic
     labelMulEntrySave(savedDicts[saveNamesList[1]], saveNamesList[1])  # Saves
-    labelEntrySave(savedDicts[saveNamesList[2]], saveNamesList[2])  # Abilities
-    statSave(savedDicts[saveNamesList[3]], saveNamesList[3])  # Stats
-    statSave(savedDicts[saveNamesList[4]], saveNamesList[4])  # Proficiencies
-    labelMulEntrySave(savedDicts[saveNamesList[5]], saveNamesList[5])  # Equipment
-    labelEntrySave(savedDicts[saveNamesList[6]], saveNamesList[6])  # Money
-    return
+    abilitySave(savedDicts[saveNamesList[2]], saveNamesList[2])  # Abilities
+    labelEntrySave(savedDicts[saveNamesList[3]], saveNamesList[3])  # Skills
+    statSave(savedDicts[saveNamesList[4]], saveNamesList[4])  # Stats
+    statSave(savedDicts[saveNamesList[5]], saveNamesList[5])  # Proficiencies
+    labelMulEntrySave(savedDicts[saveNamesList[6]], saveNamesList[6])  # Inventory
+    labelEntrySave(savedDicts[saveNamesList[7]], saveNamesList[7])  # Money
 
+
+def abilitySave(dict, name):
+    """Hardcoded ability score function to handle the checkboxes."""
+    with open(f'saveData/{name}.txt', 'w') as f:
+        abils = {}  # Remake dict as {Label : [Entry, BooleanVar]}
+        for ability in dict:
+            vList = []
+            vList.append(dict[ability][1].get())
+            vList.append(dict[ability][3].get())
+            abils[ability] = vList
+        jabils = json.dumps(abils)
+        f.write(jabils)
+    
 
 def labelEntrySave(dict, name):
     """Handles save data generation for dictionaries that contain a label, 
@@ -22,7 +38,10 @@ def labelEntrySave(dict, name):
     with open(f'saveData/{name}.txt', 'w') as f:
         basicInfo = {}  # Remake dict as {Label : Entry.get()}
         for label in dict:
-            basicInfo[label] = dict[label][1].get()
+            if isinstance(dict[label][0], BooleanVar):  # Handles skills
+                basicInfo[label] = dict[label][0].get()
+            else:
+                basicInfo[label] = dict[label][1].get()
         jbasic = json.dumps(basicInfo)
         f.write(jbasic)
     return jbasic
@@ -32,7 +51,7 @@ def labelMulEntrySave(dict, name):
     """Handles save data dictionaries with a list for the value."""
     with open(f'saveData/{name}.txt', 'w') as f:
         mulEntry = {}  # Remake dict as {Label : [List of values]}
-        for label in dict:  # 'Successes', 'Failures'.
+        for label in dict:  # Keys
             vList = []
             for chk in dict[label]:  # Multiple values
                 vList.append(chk.get())
